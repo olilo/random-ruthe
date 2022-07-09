5e4e2918beef1b1441b7578518e141fc9ddb2fbc
Oliver Lorenz <lorenz@cosmocode.de>
2022-07-09 11:48:50 +0200

    Use String padStart function

--- js/script.js
+++ js/script.js
@@ -3,10 +3,8 @@ $(function() {
 	// this is taken directly from the page, should be fetched dynamically (somehow)
 	var maxStripNumber = 2309;
         var stripNumber = Math.floor(Math.random() * maxStripNumber);
-        var stripNumberString = "" + stripNumber;
-        while (stripNumberString.length < 4) {
-            stripNumberString = "0" + stripNumberString;
-        }
+        var stripNumberString = String(stripNumber).padStart(4, '0');
+
         var img = new Image();
         img.src = "https://ruthe.de/cartoons/strip_" + stripNumberString + ".jpg";
         img.className="d-block mx-auto";
